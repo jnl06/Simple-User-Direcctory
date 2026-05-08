c27847c3957ef36d66461e7aaa6e184ae7dcc976
Jonel <jonelpronton06@gmail.com>
2026-05-08 15:44:37 +0800

    first commit

---
+++ userdirectory/src/App.js
@@ -0,0 +1,46 @@
+import {useEffect, useState} from 'react'
+import './App.css';
+
+function App() {
+  const [user, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        setUsers(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        console.log('Failed to fetch users:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (loading) <p>Loading...</p>;
+  if (error) <p>Failed to fetch users {error.message}</p>;
+
+  return (
+    <div>
+      <h1>USERS</h1>
+      <div className='users'>
+        {user.slice(0,5).map((user) => (
+          <div className='details' key={user.id}>
+            <h4>{user.name}</h4>
+            <p>Email: {user.email}</p>
+            <p>Company Name: {user.company.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
